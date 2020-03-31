5d5158776a1b329d19dc586ea4da8a8a8e27d809
fenykep <abel.fenykep@gmail.com>
2020-03-31 15:08:37 +0200

    sitemap grows

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<title>Template</title>
+	<?php include 'header.html'?>
+</head>
+<body>
+	
+	<h1>Heeyo</h1>
+	<p>Sample paragraph with a lot of meaningless text</p>
+	<div><img src="http://www.nemet-fogaszati-centrum.hu/wp-content/uploads/2016/07/munkatars-dr-szalantzy-melinda.jpg"></div>
+
+</body>
+</html>
